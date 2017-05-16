35a058f0cebdfdca6afd72242cc2df53ad568a7e
RadRush <christianolenberger@hotmail.de>
2017-05-16 22:18:34 +0200

    Added Message

--- MyApplication/app/src/main/java/com/example/kerem/myapplication/app/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/kerem/myapplication/app/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast toast = Toast.makeText(getApplicationContext(), "Hi kerem", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
